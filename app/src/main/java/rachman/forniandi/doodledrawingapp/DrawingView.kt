c22ea43ff1210ae096b673511bfc4965d40d8187
RachmanForniandi <rachmanforniandi@gmail.com>
2022-12-08 16:53:58 +0700

    improving main layout & functional drawing done.

--- app/src/main/java/rachman/forniandi/doodledrawingapp/DrawingView.kt
+++ app/src/main/java/rachman/forniandi/doodledrawingapp/DrawingView.kt
@@ -14,12 +14,19 @@ import android.view.View
 class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
 
     private var mDrawPath: CustomPath?= null
+    
     private var mCanvasBitmap: Bitmap?= null
+    
     private var mDrawPaint: Paint?= null
+    
     private var mCanvasPaint:Paint?= null
+    
     private var mBrushSize:Float =0.toFloat()
+    
     private var color = Color.BLACK
+    
     private var canvas: Canvas? = null
+    
     private val mPaths = ArrayList<CustomPath>()
 
     init {
@@ -29,10 +36,10 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
     private fun setupDrawing() {
         mDrawPaint = Paint()
         mDrawPath = CustomPath(color,mBrushSize)
-        mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mDrawPaint?.color = color
+        mDrawPaint?.style = Paint.Style.STROKE
+        mDrawPaint?.strokeJoin = Paint.Join.ROUND
+        mDrawPaint?.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         //mBrushSize = 20.toFloat()
 
@@ -44,21 +51,22 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
         canvas = Canvas(mCanvasBitmap!!)
     }
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         mCanvasBitmap?.let {
-            canvas?.drawBitmap(it,0f,0f,mCanvasPaint)
+            canvas.drawBitmap(it,0f,0f,mCanvasPaint)
         }
 
         for (path in mPaths){
-            mDrawPaint!!.strokeWidth =path.brushThickness
-            mDrawPaint!!.color = path.color
-            canvas?.drawPath(path,mDrawPaint!!)
+            mDrawPaint?.strokeWidth =path.brushThickness
+            mDrawPaint?.color = path.color
+            canvas.drawPath(path,mDrawPaint!!)
         }
-        if (mDrawPath!!.isEmpty){
-            mDrawPaint!!.strokeWidth =mDrawPath!!.brushThickness
-            mDrawPaint!!.color = mDrawPath!!.color
-            canvas?.drawPath(mDrawPath!!,mDrawPaint!!)
+
+        if (!mDrawPath!!.isEmpty){
+            mDrawPaint?.strokeWidth =mDrawPath!!.brushThickness
+            mDrawPaint?.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!,mDrawPaint!!)
         }
     }
 
@@ -93,9 +101,15 @@ class DrawingView(context: Context,attrs: AttributeSet):View(context, attrs) {
     }
 
     fun setSizeForBrush(newSize:Float){
-        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
         newSize,resources.displayMetrics)
         mDrawPaint?.strokeWidth = mBrushSize
+    }
+
+    fun setColor(newColor:String){
+        color = Color.parseColor(newColor)
+        mDrawPaint?.color = color
     }
 
     internal inner class CustomPath(var color:Int,var brushThickness:Float):Path()
--- app/src/main/java/rachman/forniandi/doodledrawingapp/MainActivity.kt
+++ app/src/main/java/rachman/forniandi/doodledrawingapp/MainActivity.kt
@@ -3,7 +3,9 @@ package rachman.forniandi.doodledrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import rachman.forniandi.doodledrawingapp.databinding.ActivityMainBinding
@@ -18,18 +20,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.drawingView.setSizeForBrush(20.toFloat())
+        //binding.drawingView.setSizeForBrush(20.toFloat())
 
         mImgButtonCurrentPaint = binding.linePaintColors[1] as ImageButton
         mImgButtonCurrentPaint?.setImageDrawable(
             ContextCompat.getDrawable(this,R.drawable.pallet_pressed))
 
         binding.imgButtonBrush.setOnClickListener {
-            showBrushSizeDialog()
+            showBrushSizeChooserDialog()
         }
     }
 
-    private fun showBrushSizeDialog() {
+    private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush Size: ")
@@ -49,5 +51,29 @@ class MainActivity : AppCompatActivity() {
             brushDialog.dismiss()
         })
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View){
+        //Toast.makeText(this,"clicked paint", Toast.LENGTH_LONG).show()
+        if (view !== mImgButtonCurrentPaint){
+            // Update the color
+            val imgButton = view as ImageButton
+            // Here the tag is used for swaping the current color with previous color.
+            // The tag stores the selected view
+            val colorTag = imgButton.tag.toString()
+            // The color is set as per the selected tag here.
+            binding.drawingView.setColor(colorTag)
+            // Swap the backgrounds for last active and currently active image button.
+            imgButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            )
+
+            mImgButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+            )
+
+            mImgButtonCurrentPaint = view
+
+        }
     }
 }
